78c0ef89a29aa9f038af61fab2f9b31d71cee0ac
jungpaeng <meis1541@naver.com>
2021-05-31 18:54:42 +0900

    ads.daangn.com: 환경 세팅

---
+++ ads.daangn.com/gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "https://ads.daangn.com",
+  },
+  plugins: [],
+};
---
+++ ads.daangn.com/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      Hello, World
+    </main>
+  )
+}
+
+export default IndexPage
